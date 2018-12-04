b7d7d7785490c0bd713fb5cdc8f38321c78a7dd5
Ben Olive <sionide21@gmail.com>
2018-12-04 07:52:03 -0500

    Helper for finding slow code

---
+++ lib/benchmark.ex
@@ -0,0 +1,23 @@
+defmodule Benchmark do
+  defmacro measure(pipe, ast) do
+    quote do
+      pipe = unquote(pipe)
+
+      Benchmark.time_function("|> " <> unquote(Macro.to_string(ast)), fn ->
+        pipe |> unquote(ast)
+      end)
+    end
+  end
+
+  defmacro measure(ast) do
+    quote do
+      Benchmark.time_function(unquote(Macro.to_string(ast)), fn -> unquote(ast) end)
+    end
+  end
+
+  def time_function(label, fun) do
+    {time, result} = :timer.tc(fun)
+    IO.puts("#{label}: #{:erlang.convert_time_unit(time, :native, :microsecond)}")
+    result
+  end
+end
