958f90867058cbd6f720703c7777fceec01bf6ca
Ben Olive <sionide21@gmail.com>
2018-12-05 08:15:18 -0500

    Mix format

--- lib/aoc/day4.ex
+++ lib/aoc/day4.ex
@@ -3,19 +3,26 @@ defmodule AOC.Day4 do
     input
     |> Enum.sort()
     |> Enum.map(&parse_line/1)
-    |> Enum.chunk_while(nil, fn
-      {_, {:gaurd, guard}}, _ -> {:cont, guard}
-      {sleep, :sleep}, guard -> {:cont, {guard, sleep}}
-      {wake, :wake}, {guard, sleep} -> {:cont, {guard, sleep, wake}, guard}
-    end, fn _ -> {:cont, nil} end)
+    |> Enum.chunk_while(
+      nil,
+      fn
+        {_, {:gaurd, guard}}, _ -> {:cont, guard}
+        {sleep, :sleep}, guard -> {:cont, {guard, sleep}}
+        {wake, :wake}, {guard, sleep} -> {:cont, {guard, sleep, wake}, guard}
+      end,
+      fn _ -> {:cont, nil} end
+    )
   end
 
   def sleep_schedules(events) do
     Enum.reduce(events, %{}, fn {guard, sleep, wake}, acc ->
       schedule = Map.get(acc, guard, %{})
-      schedule = Enum.reduce(sleep..(wake-1), schedule, fn minute, schedule ->
-        Map.update(schedule, minute, 1, &(&1 + 1))
-      end)
+
+      schedule =
+        Enum.reduce(sleep..(wake - 1), schedule, fn minute, schedule ->
+          Map.update(schedule, minute, 1, &(&1 + 1))
+        end)
+
       Map.put(acc, guard, schedule)
     end)
   end
@@ -24,6 +31,7 @@ defmodule AOC.Day4 do
     [_, time, event] =
       ~r/:(\d\d)] (.+)$/
       |> Regex.run(line)
+
     {String.to_integer(time), parse_event(event)}
   end
 
@@ -31,9 +39,11 @@ defmodule AOC.Day4 do
     {n, _} = Integer.parse(rest)
     {:gaurd, n}
   end
+
   def parse_event("falls asleep") do
     :sleep
   end
+
   def parse_event("wakes up") do
     :wake
   end
--- lib/aoc/day5.ex
+++ lib/aoc/day5.ex
@@ -6,17 +6,21 @@ defmodule AOC.Day5 do
   def do_react([a, b | rest], acc, _) when abs(a - b) == 32 do
     do_react(rest, acc, true)
   end
+
   def do_react([a, b | rest], acc, reacted?) do
     do_react([b | rest], [a | acc], reacted?)
   end
+
   def do_react([b], acc, reacted?) do
     do_react([], [b | acc], reacted?)
   end
+
   def do_react([], acc, true) do
     acc
     |> Enum.reverse()
     |> react()
   end
+
   def do_react([], acc, false) do
     Enum.reverse(acc)
   end
