941442de9815059b4180004898bcbffa92b8a9c9
Ben Olive <sionide21@gmail.com>
2018-12-06 09:53:56 -0500

    Much faster Day 6 part 1

--- lib/aoc/day6.ex
+++ lib/aoc/day6.ex
@@ -1,43 +1,43 @@
 defmodule AOC.Day6 do
   def largest_area(points) do
     points
-    |> Enum.map(&area(points, &1))
+    |> areas()
+    |> Map.values()
     |> Enum.max()
   end
 
-  def area(points, point) do
-    do_area(points, point, MapSet.new(), 0, 0)
+  def areas(points) do
+    center = center(points)
+    do_areas(points, center, %{}, 0, false)
   end
 
-  def do_area(points, point, seen, distance, total) do
-    seen_count = MapSet.size(seen)
+  def do_areas(points, point, areas, distance, done?) do
+    new_areas =
+      point
+      |> square(distance)
+      |> Enum.reduce(areas, fn spot, areas ->
+        points
+        |> owner(spot)
+        |> case do
+          nil ->
+            areas
 
-    point
-    |> square(distance)
-    |> Enum.reduce({0, seen}, fn spot, {count, seen} ->
-      points
-      |> owner(spot)
-      |> case do
-        nil ->
-          {count, seen}
-
-        ^point ->
-          {count + 1, MapSet.put(seen, point)}
-
-        other ->
-          {count, MapSet.put(seen, other)}
-      end
-    end)
-    |> case do
-      {0, _} ->
-        total
-
-      {n, seen} ->
-        if MapSet.size(seen) == seen_count && MapSet.size(seen) == Enum.count(points) do
-          -1
-        else
-          do_area(points, point, seen, distance + 1, total + n)
+          owner ->
+            Map.update(areas, owner, 1, &(&1 + 1))
         end
+      end)
+
+    cond do
+      done? ->
+        areas
+        |> Enum.filter(fn {owner, area} -> new_areas[owner] == area end)
+        |> Enum.into(%{})
+
+      Enum.count(new_areas) == Enum.count(points) ->
+        do_areas(points, point, new_areas, distance + 1, true)
+
+      true ->
+        do_areas(points, point, new_areas, distance + 1, false)
     end
   end
 
@@ -57,6 +57,18 @@ defmodule AOC.Day6 do
       end)
 
     owner
+  end
+
+  def center(points) do
+    size = Enum.count(points)
+
+    {sumx, sumy} =
+      points
+      |> Enum.reduce(fn {x, y}, {sumx, sumy} ->
+        {x + sumx, y + sumy}
+      end)
+
+    {round(sumx / size), round(sumy / size)}
   end
 
   def distance({x1, y1}, {x2, y2}) do
